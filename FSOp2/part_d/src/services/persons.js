dd3aeb83efc5f4aa15f3756175c8bfebb20069d6
Chirag <chiragtrivedi17@hotmail.com>
2023-06-02 23:22:47 -0400

    e2.12

---
+++ FSOp2/part_d/src/services/persons.js
@@ -0,0 +1,19 @@
+// persons communicating with backend server
+
+import axios from 'axios'
+const baseUrl = 'http://localhost:3001/persons'
+
+const getAll = () => {
+  // defining the promise as request
+  const request = axios.get(baseUrl)
+  // sending then promise result
+  return request.then(response => response.data)
+
+}
+
+const create = newObject => {
+  const request = axios.post(baseUrl, newObject)
+  return request.then(response => response.data)
+}
+
+export default { getAll, create}
