96b7c63b37474cf1776860862c593334be25abfa
Chirag <chiragtrivedi17@hotmail.com>
2023-06-27 23:40:35 -0400

    e3.1

---
+++ FSOp3/part_a/index.js
@@ -0,0 +1,39 @@
+// index.js
+
+const express = require('express')
+const app = express()
+
+app.unsubscribe(express.json())
+
+let phonebookPersons = [
+  { 
+    "id": 1,
+    "name": "Arto Hellas", 
+    "number": "040-123456"
+  },
+  { 
+    "id": 2,
+    "name": "Ada Lovelace", 
+    "number": "39-44-5323523"
+  },
+  { 
+    "id": 3,
+    "name": "Dan Abramov", 
+    "number": "12-43-234345"
+  },
+  { 
+    "id": 4,
+    "name": "Mary Poppendieck", 
+    "number": "39-23-6423122"
+  }
+]
+
+app.get('/api/persons', (request, response) => {
+  response.json(phonebookPersons)
+})
+
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
